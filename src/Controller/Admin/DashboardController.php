2fbbdba658822a17f96c1c9302b74f0d0bbdb6f7
VipapkStudios <6005156@mborijnland.nl>
2022-01-28 13:17:45 +0100

    commit failed

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -31,12 +31,44 @@ class DashboardController extends AbstractDashboardController
         //
         // return $this->render('some/path/my-dashboard.html.twig');
     }
-
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Symfonytest');
+            // the name visible to end users
+            ->setTitle('Symfonytest')
+            // you can include HTML contents too (e.g. to link to an image)
+            ->setTitle('<img src="..."> ACME <span class="text-small">Corp.</span>')
+
+            // the path defined in this method is passed to the Twig asset() function
+            ->setFaviconPath('favicon.svg')
+
+            // the domain used by default is 'messages'
+            ->setTranslationDomain('my-custom-domain')
+
+            // there's no need to define the "text direction" explicitly because
+            // its default value is inferred dynamically from the user locale
+            ->setTextDirection('ltr')
+
+            // set this option if you prefer the page content to span the entire
+            // browser width, instead of the default design which sets a max width
+            // ->renderContentMaximized()
+
+            // set this option if you prefer the sidebar (which contains the main menu)
+            // to be displayed as a narrow column instead of the default expanded design
+            //->renderSidebarMinimized()
+
+            // by default, all backend URLs include a signature hash. If a user changes any
+            // query parameter (to "hack" the backend) the signature won't match and EasyAdmin
+            // triggers an error. If this causes any issue in your backend, call this method
+            // to disable this feature and remove all URL signature checks
+            //->disableUrlSignatures()
+
+            // by default, all backend URLs are generated as absolute URLs. If you
+            // need to generate relative URLs instead, call this method
+            //->generateRelativeUrls()
+        ;
     }
+
 
     public function configureMenuItems(): iterable
     {
