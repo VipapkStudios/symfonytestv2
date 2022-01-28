1b1416a268bd73a6b66abd5c9b903a3057a7a80f
VipapkStudios <6005156@mborijnland.nl>
2022-01-28 12:38:44 +0100

    commit

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -6,20 +6,19 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ProductType extends AbstractType
+class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('price')
-            ->add('description');
+            ->add('field_name')
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Product::class,
+            // Configure your form options here
         ]);
     }
 }
